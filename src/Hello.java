a8f8766e15b43225ba9ca028ddfcf1a29f5f2719
KHUSHALNETAKE <khushalnetake02@gmail.com>
2019-03-30 03:06:04 -0700

    chanes by eclipse

--- src/Hello.java
+++ src/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
   System.out.println(" 1.Hello world");
   System.out.println(" 2.welcome to git khushal");
   System.out.println(" 3.enjoing git workshop");
+  System.out.println(" 4.changes from eclips");
 	}
 
 }
